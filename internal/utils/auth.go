38dd22fc88eae1afe7888b22a2b2046ad21304a4
Luiz Silva <lfmachadodasilva@gmail.com>
2025-06-20 14:27:28 +0100

    always retry trakt

--- internal/utils/auth.go
+++ internal/utils/auth.go
@@ -1,9 +1,5 @@
 package utils
 
-import (
-	"strings"
-)
-
 // IsAuthError checks if the given error is an authentication error.
 // This function assumes that authentication errors are HTTP errors with a 401 status code.
 func IsAuthError(err error) bool {
@@ -12,5 +8,6 @@ func IsAuthError(err error) bool {
 	}
 
 	// Check if the error string contains "401"
-	return strings.Contains(err.Error(), "401")
+	// return strings.Contains(err.Error(), "401") || strings.Contains(err.Error(), "400")
+	return true
 }
