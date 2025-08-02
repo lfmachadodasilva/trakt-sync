22e44070dc6a0c7998e3d089a2d48b34ed68a136
Luiz Felipe Machado da Silva <lfmachadodasilva@gmail.com>
2025-08-02 11:25:57 +0100

    read & write database

--- src/app/api/sync/route.ts
+++ src/app/api/sync/route.ts
@@ -1,6 +1,19 @@
-import { getDatabase } from "@/database";
+import { getConfig, getDatabase, saveConfig } from "@/database";
+import { Config } from "@/database/model";
 
 export async function GET(request: Request) {
-  const db = await getDatabase();
+  //   await saveConfig({
+  //     plex: {
+  //       userId: "test plex",
+  //     },
+  //     trakt: {
+  //       clientId: "test trakt",
+  //     },
+  //     emby: {
+  //       userId: "test emby",
+  //     },
+  //   } as Config);
+  console.log(getConfig());
+
   return Response.json({});
 }
--- src/database/config.ts
+++ src/database/config.ts
@@ -1,10 +1,8 @@
+import { EmbyConfig } from "@/clients/emby";
 import { getDatabase } from ".";
 import { Config } from "./model";
-
-interface ConfigRaw {
-  key: string;
-  value: string;
-}
+import { TraktConfig } from "@/clients/trakt";
+import { PlexConfig } from "@/clients/plex";
 
 export const getConfig = async (): Promise<Config> => {
   const db = await getDatabase();
@@ -12,11 +10,37 @@ export const getConfig = async (): Promise<Config> => {
   const cfgRaw = await db.all<{ key: string; value: string }[]>(
     "select * from config"
   );
-  return {} as Config;
+
+  console.log(cfgRaw);
+
+  const cfg: Config = {
+    emby:
+      (JSON.parse(
+        cfgRaw.find((x) => x.key == "emby")?.value ?? ""
+      ) as EmbyConfig) ?? {},
+    trakt:
+      (JSON.parse(
+        cfgRaw.find((x) => x.key == "trakt")?.value ?? ""
+      ) as TraktConfig) ?? {},
+    plex:
+      (JSON.parse(
+        cfgRaw.find((x) => x.key == "plex")?.value ?? ""
+      ) as PlexConfig) ?? {},
+  };
+
+  console.log(cfg);
+
+  return cfg;
 };
 
-export const saveConfig = async (config: Config) => {
+export const saveConfig = async (cfg: Config) => {
   const db = await getDatabase();
 
-  await db.run("INSERT INTO config (key, value) VALUES (?, ?)", []);
+  const stmt = await db.prepare(
+    `INSERT OR REPLACE INTO config (key, value) VALUES (?, ?)`
+  );
+
+  Object.entries(cfg).forEach(async ([key, value]) => {
+    await stmt.run(key, JSON.stringify(value) ?? {});
+  });
 };
